d54cf133b6386a35baf4c45d7badaf6a4335dbae
zyros123xyz <thuan123xyz@gmail.com>
2021-01-03 18:03:18 +0700

    Upload Project first time

---
+++ demo_web_07/demo_web_07/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace demo_web_07
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ demo_web_07/demo_web_07/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="demo_web_07.MvcApplication" Language="C#" %>
